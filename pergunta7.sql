74234f0699dd037d248670c8e0a62860c9db8af5
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2023-01-13 09:16:10 -0300

    Delete pergunta7.sql

--- pergunta7.sql
+++
@@ -1,5 +0,0 @@
-select * from personagens;
-
-/* top 5 personagens que apareceram entre 2012 e 2015 em got*/
-
-SELECT personagem, atores, primeira_aparição, ultima_aparição FROM personagens WHERE primeira_aparição >= 2012 and ultima_aparição = 2015 order by primeira_aparição limit 5;
