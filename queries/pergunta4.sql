589b25ca9244756ef8c8d37b50d9ab897d87100d
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2023-01-16 08:32:17 -0300

    Update pergunta4.sql

--- queries/pergunta4.sql
+++ queries/pergunta4.sql
@@ -1,5 +1,5 @@
 select * from episodios;
 
-/* quantos minutos de duração existem em todas as temporadas de got */
+/* quantos horas de duração existem em todas as temporadas de got */
 
 SELECT SUM(duração) AS total_duração FROM episodios;
