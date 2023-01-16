2975cf1513048c84f3b231ddb5df7b1b6e06afd5
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2023-01-15 22:33:15 -0300

    Update pergunta6.sql

--- queries/pergunta6.sql
+++ queries/pergunta6.sql
@@ -1,5 +1,5 @@
 select * from personagens;
 
-/* 5 atores que mais aparecem em got */
+/* 5 personagens que mais aparecem em got */
 
 SELECT personagem, atores, aparições FROM personagens order by aparições DESC limit 5;
